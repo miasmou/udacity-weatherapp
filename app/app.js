11a15123b0fc4c9cb91036d6f308e3da916a49e0
miasmou <56071718+miasmou@users.noreply.github.com>
2021-05-09 21:05:50 +0100

    Added fikes

---
+++ app/app.js
@@ -0,0 +1,129 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey = "&appid=827ebfb15db96930699ac7a161159fa7";
+// URL for calling the API
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const units = "&units=metric";
+
+const zipInput = document.getElementById('zip');
+const feelingInput = document.getElementById('feelings');
+const entriesContainer = document.getElementById('entriesContainer');
+
+/* Function called by event listener */
+const submitEntry = () => { 
+    //Get today's date
+    const date = new Date();
+    //Format it in a user-friendly way
+    const formattedDate = date.toDateString();
+
+    // Capture value of textarea before clearing it
+    const userFeeling = feelingInput.value;
+
+    getWeatherAtCity(baseURL, zipInput.value)
+    .then(function(data){
+        postEntry('/addEntry', {date: formattedDate, city: data.name, weather: data.weather, temp: data.main.temp, feeling: userFeeling})
+        .then(
+            getEntries('/allEntries')    
+        )
+    })
+
+    //Clear inputs
+    zipInput.value = null;
+    feelingInput.value = null;
+}
+
+/* GET Weather info */
+const getWeatherAtCity = async (baseURL, city) => {
+    const url = baseURL + city + apiKey + units;
+    const result = await fetch(url)
+    try{
+        const data = await result.json();  
+        return data;
+    } catch(error){
+        console.log('error', error);
+    }
+}
+
+/* POST Weather info + date + feeling to app */
+const postEntry = async (url, data) => {
+    const result = await fetch(url, {
+        method:'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try{
+        const newData = await result.json();
+        return newData;
+    } catch(error){
+        console.log('error', error);
+    }
+}
+
+/* GET All app Entries */
+const getEntries = async (url) => {
+    const result = await fetch(url)
+    try{
+        const entries = await result.json();
+        console.log(entries);
+        displayEntries(entries);
+    } catch(error){
+        console.log('error', error);
+    }
+}
+
+/* Update UI with entries */
+const displayEntries = (entries) => {
+    const fragment = document.createDocumentFragment();
+    entriesContainer.innerHTML = null;
+    if(entries.length == 0){
+        // Empty state if no entries
+        createElementWithText(entriesContainer, 'p', "No entries to display", 'entries__placeholder');
+    } else{
+        // Iterate through entries and build entry element for each
+        for(entry of entries){
+            // Build out entry structure
+            const newEntryContainer = document.createElement('div');
+            newEntryContainer.classList.add('entryHolder');
+            const newEntryDate = document.createElement('div');
+            newEntryDate.classList.add('date');
+            const newEntryTemp = document.createElement('div');
+            newEntryTemp.classList.add('temp');
+            const newEntryContent = document.createElement('div');
+            newEntryContent.classList.add('content');
+    
+            // Add elements with text data
+            createElementWithText(newEntryContainer, 'h3', entry.city, 'entry__header');
+            createElementWithText(newEntryDate, 'p', entry.date, 'entry__date');
+            createElementWithText(newEntryTemp, 'p', "Weather: " + entry.weather[0].main, 'entry__weather');
+            createElementWithText(newEntryTemp, 'p', "Temperature: " + entry.temp +'°C', 'entry__temp');
+            createElementWithText(newEntryContent, 'p', "You were feeling: " + entry.feeling, 'entry__feeling');
+    
+            newEntryContainer.appendChild(newEntryDate);
+            newEntryContainer.appendChild(newEntryTemp);
+            newEntryContainer.appendChild(newEntryContent);
+            fragment.appendChild(newEntryContainer);
+        }
+        entriesContainer.appendChild(fragment);
+    }
+}
+
+// Utility function to create elements with text inside and a custom class
+const createElementWithText = (container, type, innerText, classToAdd) => {
+    // Create element
+    const elementToCreate = document.createElement(type);
+    // Set inside text
+    elementToCreate.innerText = innerText;
+    // Add appropriate classes
+    elementToCreate.classList.add(classToAdd);
+    // Append to container
+    container.appendChild(elementToCreate);
+}
+
+// Get Entries on start
+getEntries('/allEntries');
+
+// Event listener to add function to button
+document.getElementById("generate").addEventListener("click", submitEntry);
+
---
+++ server.js
@@ -0,0 +1,50 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// App endpoint
+const projectData = [];
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize client side folder
+app.use(express.static('app'));
+
+// GET Route
+app.get('/allEntries', function (req, res) {
+    res.send(projectData);
+});
+
+// POST Route
+app.post('/addEntry', addEntry);
+
+function addEntry(req, res){
+    console.log(res);
+
+    newEntry = {
+        date: req.body.date,
+        city: req.body.city,
+        weather: req.body.weather,
+        temp: req.body.temp,
+        feeling: req.body.feeling
+    }
+
+    projectData.push(newEntry);
+    res.send(projectData);
+}
+
+
+const port = 8000;
+const server = app.listen(port, () => {console.log(`running on localhost: ${port}`)});
